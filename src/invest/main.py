bcfce4194200cafaf48ef8eeb070b062f24ee2aa
lmkirvan <kirvan.lewis@gmail.com>
2025-12-09 11:51:59 -0500

    initial commit

---
+++ src/invest/main.py
@@ -0,0 +1,30 @@
+import typer
+from rich import print
+from typing import Annotated
+import random
+
+data  = {
+        "name" : "Rick",
+        "age" : 42,
+        "items" : [{"name": "Portal Gun"}, {"name": "Plumbus"}],
+        "active": True,
+        "affiliation": None
+}
+
+app = typer.Typer()
+
+# you can use a function or a just a variable to provide default arguments 
+def get_name() -> str :
+    return random.choice(["Dave", "Lewis", "Beatrice"])
+
+# arguments should probably get their own data container thing? 
+@app.command()
+def main(name: Annotated[str, typer.Argument(default_factory=get_name, help="provide a name for a personalized greeting")]):
+
+    print(f"Hello, {name}, here's the data")
+    print(data)
+
+
+if __name__ == "__main__":
+    app()
+
