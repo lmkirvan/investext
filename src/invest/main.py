9d4e0510977c044727ec4e94d70c8210943600e2
lmkirvan <kirvan.lewis@gmail.com>
2025-12-15 17:07:47 -0500

    kinda got it working, just need to get it into a database

--- src/invest/main.py
+++ src/invest/main.py
@@ -1,15 +1,26 @@
 import typer
-from typing import List, Optional, Annotated, Any
+from typing import List, Annotated, Any
 from rich import print
 from pathlib import Path
+import json 
+
 
 app  = typer.Typer()
 
 def bin_mask(l: List[Any], p: list[bool]):
     assert len(l) == len(p)
-
     return [item for item, keep in zip(l, p) if keep] 
 
+
+def read_whole_folder(root: str, items: List[str]):
+    res = {} 
+    p = Path(root)
+    for i in items:
+        text = (p / i).read_text()
+        res[i] = text
+
+    return res
+    
 
 @app.command(name = 'setup')
 def setup(
@@ -32,22 +43,22 @@ def setup(
         v = None
  
     structure = {}
-    for root, dir, file in path.walk(on_error=v):
+    for root, _, file in path.walk(on_error=v):
 
         flgl = [True if f.endswith(extension) else False for f in file]
         structure[str(root)] = bin_mask(file, flgl)
-        # we onlyl care about the roots with some kind of text files in them 
+    # we onlyl care about the roots with some kind of text files in them 
+    structure = {k: v for k,v in structure.items() if len(v) != 0}  
 
-    structure = {k: v for k,v in structure.items() if len(v) == 0} 
+    res = {}
 
-    print(len(structure))
-    
-
-
-
+    for k,v in structure.items():
+        res[k] = read_whole_folder(k, v)
 
 
-
+    with open("data.json", "w") as f:
+        json.dump(res, f)
+ 
 @app.callback()
 def create():
     pass
